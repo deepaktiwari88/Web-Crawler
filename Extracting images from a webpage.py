016caf8bfefe99a0c1299f6c7696ee5040c1f880
Deepak Tiwari <tiwarid99@gmail.com>
2017-09-01 19:07:30 +0530

    Update Extracting images from a webpage.py
    The code is updated so that it checks whether the image belongs to a particular department(Drawings and Prints) and then download it.

--- Extracting images from a webpage.py
+++ Extracting images from a webpage.py
@@ -1,44 +1,70 @@
+
+""" Before running please create a .txt file containing the required links in the same directory
+    as this file """
+
 from urllib import request
 from html.parser import HTMLParser
 from urllib import parse
 
-links=[]
-f = open('a.txt','r')   # Replace a.txt with appropriate file name that contains links of webpages
+links = []
+f = open('links.txt', 'r')  #Replace a.txt with appropriate file name
 for line in f:
-    a = line.replace('\n','')
+    a = line.replace('\n', '')
     links.append(a)
 
 f.close()
 
-class Download_images(HTMLParser):
+class check(HTMLParser):
+    def __init__(self, url):
+        super().__init__()
+        self.url = url
+        self.flag=0
 
-    def __init__(self,url,count):
+    def handle_data(self, data):
+        if data.find('Drawings and Prints')!=-1:        #Downloading images with this department
+            self.flag = 1
+
+    def get_flag(self):
+        return self.flag
+
+class Download_images(HTMLParser):
+    def __init__(self, url, count):
         super().__init__()
         self.count = count
         self.url = url
 
     def handle_starttag(self, tag, attrs):
-        if tag=='img':
-            for name,value in attrs:
-                if name=='src':
-                    address = parse.urljoin(self.url,value).split(" ")[0]
+        if tag == 'img':
+            for name, value in attrs:
+                if name == 'srcset':
+                    a = parse.urljoin(self.url, value).split(" ")[6]
+                    address = parse.urljoin(self.url, a)
                     #print(address)
-                    name = str(self.count)+'.jpg'
-                    request.urlretrieve(address,name)
-                    self.count +=1
+                    name = str(self.count) + '.jpg'
+                    request.urlretrieve(address, name)
+                    self.count += 1
 
     def get_count(self):
         return self.count
 
-count=1
+count = 1
 
 for url in links:
-    if url=='':
+    flag=0
+    if url == '':
         break
-    response = request.urlopen(url)
-    bytes = response.read()
-    string = bytes.decode('utf-8')
-    #print(string)
-    downloader = Download_images(url,count)
-    downloader.feed(string)
-    count = downloader.get_count()
+    try:
+        #print(url)
+        response = request.urlopen(url)
+        bytes = response.read()
+        string = bytes.decode('utf-8')
+        checker = check(url)
+        checker.feed(string)
+        flag = checker.get_flag()
+        #print(flag)
+        if flag==1:
+            downloader = Download_images(url, count)
+            downloader.feed(string)
+            count = downloader.get_count()
+    except:
+        print("No image available")
